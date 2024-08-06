a1017e0df51f40b889e8c5e31790f7af2f863178
Charlie Nader <nitro.naj@gmail.com>
2024-08-06 13:21:45 -0700

    add delay function in test file

--- src/calc.test.ts
+++ src/calc.test.ts
@@ -1,8 +1,7 @@
 import { add } from "./calc";
 
-
-describe('Add function', () => {
-  it('should test add function', () => {
+describe("Add function", () => {
+  it("should test add function", () => {
     expect(add(0, 0)).toBe(0);
     expect(add(1, 0)).toBe(1);
     expect(add(1, 1)).toBe(2);
@@ -14,3 +13,7 @@ describe('Add function', () => {
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
 });
+
+async function delay(delayTime: number = 1000): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, delayTime));
+}
